2ee342f09b7d3402b6b948c0a16f3ad2cb23247e
k-rathi <kunalrathi1994@gmail.com>
2018-07-28 12:29:43 -0400

    fixed payment

--- client/src/pages/Buy-components/PaymentRequest.js
+++ client/src/pages/Buy-components/PaymentRequest.js
@@ -8,13 +8,15 @@ class PaymentRequestForm extends React.Component {
       country: 'US',
       currency: 'usd',
       total: {
-        label: 'Demo total',
         amount: 1000,
+        label: 'Demo total',
       },
     });
 
     paymentRequest.on('token', ({complete, token, ...data}) => {
+          // tslint:disable-next-line:no-console
       console.log('Received Stripe token: ', token);
+          // tslint:disable-next-line:no-console
       console.log('Received customer information: ', data);
       complete('success');
     });
@@ -32,14 +34,14 @@ class PaymentRequestForm extends React.Component {
   render() {
     return this.state.canMakePayment ? (
       <PaymentRequestButtonElement
-        paymentRequest={this.state.paymentRequest}
         className="PaymentRequestButton"
+        paymentRequest={this.state.paymentRequest}
         style={{
           // For more details on how to style the Payment Request Button, see:
           // https://stripe.com/docs/elements/payment-request-button#styling-the-element
           paymentRequestButton: {
-            theme: 'light',
             height: '64px',
+            theme: 'light',
           },
         }}
       />
